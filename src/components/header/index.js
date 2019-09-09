80460253559f9c5806779ab23d0d03cdce25d3fc
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-09-08 22:41:15 -0300

    add header how component to use in the main page

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import React from 'react';
+import './style.css';
+
+const Header = () =>{
+    return <header id="main-header"> <span>I am a header</span> </header>
+}
+
+export default Header;
