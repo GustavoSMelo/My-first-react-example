e1ac2ad82e6ac565387a94b258c6d561e3b97dbe
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-09-08 22:39:57 -0300

    referencing Some components to view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
+"use strict";
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/index';
+//import api from './services/api';
+import Main from './views/main/index';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
+        <Main />
       </header>
     </div>
   );
